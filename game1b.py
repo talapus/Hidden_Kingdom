b36c3e1dd9c1143bbb6b0d0192d903993d6c4aae
jakesme0429 <Seahawks0429@gmail.com>
2018-10-10 13:24:31 -0700

    Added name function for player to choose their own name

---
+++ game1b.py
@@ -0,0 +1,54 @@
+
+def choice2(Item1, Item2, Item3, Item4, I):
+    
+        #This function chooses between two new and one existing option
+        #The item that is being assigned this option will be Item1
+        #The new items that can be selected are Item2 and Item3
+        #The existing old value in Item1 is Item4
+        #I is just a counter that will assing Item4 to Item1 when I has a value of 2
+        #If I starts at 1 then the user has two tries to type in a correct answer.
+        
+    print("\nWhat's your ",Item1,"? ",Item2," or ",Item3,"?",sep ='')
+    I1 = input()
+    I1 = I1.lower()
+
+    if  (I1 == Item2 or I1 == Item3): 
+        print ('\nGreat!')
+        
+    elif (I == 1):
+        print ("That item is not in the chest")
+        I1 = choice2 (Item1,Item2,Item3,Item4,I+1)
+        
+    else:
+        if  (I1 == Item2 or I1 == Item3):
+            print ('\nGreat!')
+        else:
+            print("Your Greed and stupidity has betrayed you.  Your ",Item1,"will be ",Item4)
+            I1=Item4
+    return I1
+
+
+#main
+Weapon_1 = 'Fists'
+Sidearm = 'None'
+
+print ('\nYour Main weapon is ' + Weapon_1 + '. And your sidearm will be ' + Sidearm)
+
+print('\nYou awake in a room...\nyou find a chest!')
+print ('\nThe chest has 4 weapons, 2 main weapons and 2 sidearms!')
+print ('the 2 main weapons are a great sword and a magic staff...')
+print ('the 2 sidearms are a bow and a bag of fairy dust...')
+
+chest_Item1 = 'great sword'
+chest_Item2 = 'magic staff'
+chest_Item3 = 'bow'
+chest_Item4 = 'fairy dust'
+
+Weapon_1 = choice2 ("Main weapon",chest_Item1,chest_Item2,Weapon_1,1)
+Sidearm = choice2 ("Sidearm",chest_Item3,chest_Item4,Sidearm,1)
+       
+print ('\n\nyour main weapon is %s and your sidearm is %s' %(Weapon_1,Sidearm) )
+a = input("Press any key to exit")
+
+
+ 
--- kingdom.py
+++ kingdom.py
@@ -4,10 +4,25 @@ from random import choice
 from time import sleep
 
 # global variables go below that
-weapon_1 = 'Fists'
-sidearm = 'None'
+weapon_1 = 'fists'
+secondary = 'none'
+
 
 # next define global functions. 
+
+def character_name():   #allows player to choose their own name
+    global player_name
+    player_name = input('\nWhat is your name?')
+    
+    while True:
+        if len(player_name) >= 2 and len(player_name) <= 15:
+            print('\nHello ' + player_name + '.')
+            break
+
+        else:
+            print ('\nYour name must be at least two characters and at most 15. ')
+            player_name = input('\nWhat is your name?')
+    return player_name
 
 def character_class():  # returns a random word from the list when the function is called
     return choice(['warrior', 'wizard', 'thief', 'cleric', 'ranger', 'paladin', 'death knight', 'monk'])
@@ -22,17 +37,19 @@ def character_race():  # is a way to do what we did above without the 'choice' m
 
 # then the main function. 
 
-if __name__ == '__main__':
+if __name__ == '__main__':   
 
-    print ('\nYour Main weaopon is ' + weapon_1 + '. And your sidearm will be ' + sidearm)
+    character_name() 
+
+    print ('\nYour Main weaopon is ' + weapon_1 + '. And your secondary is ' + secondary)
 
     # '\n' is a 'newline'. Instead of a separate "print('')" statement, just add \n to your string wherever you want the 
     # new line. I did some of it for you. 
     # For example, if you wanted three newlines after "Hello!", it would be: print("Hello!\n\n\nGoodbye!")
 
     print('\nYou awake in a room...\nyou find a chest!')
-    print('\nThe chest has 4 weapons, 2 main weapons and 2 sidearms!')
-    print('\nthe 2 main weapons are a great sword and a magic staff...\nthe 2 sidearms are a bow and a bag of fairy dust...')
+    print('\nThe chest has 4 weapons, 2 main weapons and 2 secondary weapons!')
+    print('\nthe 2 main weapons are a great sword and a magic staff...\nthe 2 secondarys are a bow and a bag of fairy dust...')
 
     chest_item1 = 'great sword'  # variable names should preferably always be lower case. I lowercased your names for you, 
     chest_item2 = 'magic staff'
@@ -42,29 +59,24 @@ if __name__ == '__main__':
     weapon_1 = input('\nWhat\'s your main weapon? Great sword or magic staff? ')
     weapon_1 = weapon_1.lower()
 
-    if  weapon_1 == chest_item1 or weapon_1 == chest_item2: 
-        print('')  # delete this line
-        print ('Great!')  # change this to:  print ('\nGreat!')
-                          # do the same thing for the others below, then delete these comments
-        sidearm = input('What will you hold as a sidearm? A bow or fairy dust? ')
-        sidearm = sidearm.lower()
+    if  weapon_1 == chest_item1 or weapon_1 == chest_item2:  
+        print ('\nGreat!')  
+                          
+        secondary = input('What will you hold as a secondary? A bow or fairy dust? ')
+        secondary = secondary.lower()
 
-        if sidearm == chest_item3 or sidearm == chest_item4 :
-                print ('')
-                print ('nice choice!')
+        if secondary == chest_item3 or secondary == chest_item4 :
+                print ('\nnice choice!')
 
         else:
-                print ('')
-                print ('Thats not in the chest try again')
-                sidearm = input('What will you hold as a sidearm? A bow or fairy dust? ')
+                print ('\nThats not in the chest try again')
+                secondary = input('What will you hold as a secondary? A bow or fairy dust? ')
 
-                if sidearm == chest_item3 or sidearm == chest_item4 :
-                    print ('')
-                    print ('nice choice!')
+                if secondary == chest_item3 or secondary == chest_item4 :
+                    print ('\nnice choice!')
 
                 else:
-                    print ('')
-                    print ('Your Greed and stupidity has betrayed you. Now you have nothing.')
+                    print ('\nYour Greed and stupidity has betrayed you. Now you have nothing.')
 
     else:
         print ('\nThe chest does not contain these items try again\n')
@@ -73,39 +85,40 @@ if __name__ == '__main__':
 
         if weapon_1 == chest_item1 or weapon_1 == chest_item2:
             print ('\ngreat')
-            sidearm = input('What will you hold as a sidearm? A bow or fairy dust? ')
-            sidearm = sidearm.lower()
+            secondary = input('What will you hold as a secondary? A bow or fairy dust? ')
+            secondary = secondary.lower()
 
-            if sidearm == chest_item3 or sidearm == chest_item4 :
+            if secondary == chest_item3 or secondary == chest_item4 :
                 print ('\nnice choice!')
 
             else:
                 print ('\nThats not in the chest try again')
-                sidearm = input('What will you hold as a sidearm? A bow or fairy dust? ')
-                sidearm = sidearm.lower()
+                secondary = input('What will you hold as a secondary? A bow or fairy dust? ')
+                secondary = secondary.lower()
 
-                if sidearm == chest_item3 or sidearm == chest_item4 :
+                if secondary == chest_item3 or secondary == chest_item4 :
                     print ('\nnice choice!')
 
                 else:
                     print ('\nYour Greed and stupidity has betrayed you. Now you have nothing.')
-                    weapon_1 = 'Fists'
-                    sidearm = 'none'
+                    weapon_1 = 'fists'
+                    secondary = 'none'
 
         else:
             print('\nyour greed and stupidity has betrayed you. Now you have nothing')
-            weapon_1 = 'Fists'
-            sidearm = 'none'
+            weapon_1 = 'fists'
+            secondary = 'none'
 
     print('ok, here we go!')
-    sleep(1)            # sleeps for 1 second
+    sleep(1.5)            # sleeps for 1.5 second
     print('\n' * 200)   # prints a newline 200 times
+    print ('Your name is ' + player_name)
     print('You are a level {} {} {}'.format(randint(1,10), character_race(), character_class())) 
                                     # python3-style print statements usually utilize the .format idiom
                                     # it is used wherever strings are used, not just print statements
                                     # character_race() and character_class() are function calls
                                                             
-    print ('\nyour main weapon is %s and your sidearm is %s' %(weapon_1, sidearm) )  # this is the python2 style way, still useful
+    print ('\nyour main weapon is a %s and your secondary is %s' %(weapon_1, secondary) )  # this is the python2 style way, still useful
 
     print('💀 ☠ 🔥 💪 💰 💡 💣 💥 🌞 🌠') # these are unicode characters. Think these might be useful to display your character?
                                        # visit https://unicode-table.com to peruse the whole unicode character set
